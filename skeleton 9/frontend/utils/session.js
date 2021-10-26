e33221bb43734b464e98f0fa7b0bd795ca3c9ca3
mrossant <matteorossant@gmail.com>
2021-10-26 09:48:54 -0400

    created api utils for creating user, session and deleting session. Also created thunk actions and session reducer

---
+++ skeleton 9/frontend/utils/session.js
@@ -0,0 +1,25 @@
+// creating user
+export const postUser = user => (
+    $.ajax({
+        url: '/api/users',
+        method: 'POST',
+        data: { user }
+    })
+);
+
+// creating session
+export const postSession = user => (
+    $.ajax({
+        url: '/api/session',
+        method: 'POST',
+        data: { user }
+    })
+);
+
+// deleting a user
+export const deleteSession = () => (
+    $.ajax({
+        url: '/api/session',
+        method: 'DELETE',
+    })
+);
